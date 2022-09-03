580add71ef4609c88083893272bff3dbf7178bf4
David Mendoza <d.mendoza99@unitec.edu>
2022-09-03 08:51:11 -0600

    final info from report

--- scripts/generate_report.py
+++ scripts/generate_report.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import shutil
+import numpy as np
 import pandas as pd
 sys.path.append("..")
 from args import get_parser
@@ -17,15 +18,18 @@ def generate_line_chart_image(title, x, label_x, filename):
     plt.savefig(filename)
     plt.clf()
 
+def remove_minus_ones(numpy_list):
+    return numpy_list[~np.isin(numpy_list, [-1])]
+
 def main():
     report = pd.read_csv(opts.report_filename)
 
     batch_size_col = report["batch_time"].to_numpy()
     train_loss_col = report["train_loss"].to_numpy()
-    medR_col = report["medR"].to_numpy()
-    R_at_1_col = report["R@1"].to_numpy()
-    R_at_5_col = report["R@5"].to_numpy()
-    R_at_10_col = report["R@10"].to_numpy()
+    medR_col = remove_minus_ones(report["medR"].to_numpy())
+    R_at_1_col = remove_minus_ones(report["R@1"].to_numpy())
+    R_at_5_col = remove_minus_ones(report["R@5"].to_numpy())
+    R_at_10_col = remove_minus_ones(report["R@10"].to_numpy())
 
     # TODO: Buscar la libreria para imprimir bomnito el delta
     if os.path.isdir('../report'):
@@ -38,6 +42,12 @@ def main():
         f.write(f"Average epoch took: {format_timespan(average_epoch_time)} \n")
         f.write(f"Highest train loss: {train_loss_col.max()} \n")
         f.write(f"Lowest train loss: {train_loss_col.min()} \n")
+        f.write(f"medR: {medR_col[-1]} \n")
+        f.write(f"R@1: {R_at_1_col[-1]} \n")
+        f.write(f"R@5: {R_at_5_col[-1]} \n")
+        f.write(f"R@10: {R_at_10_col[-1]} \n")
+        f.write(f"Average Loss: {train_loss_col[-1]} \n")
+        
 
     generate_line_chart_image("Loss Curve", train_loss_col.tolist(), "Train Loss", "../report/loss_curve.png")
     generate_line_chart_image("medR over the validation", medR_col.tolist(), "Median Rank", "../report/medR.png")
@@ -46,8 +56,5 @@ def main():
     generate_line_chart_image("R@10 over the validation", R_at_10_col.tolist(), "Recall at top 10", "../report/r_at_10.png")
     
 
-    
-
-    
 if __name__ == "__main__":
     main()
